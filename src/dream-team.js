ce2b958198e476a3216d79d31587afeb527c59b7
Dzmitry Hlushak <hlushak.dzmitry@gmail.com>
2020-02-20 13:08:46 +0300

    feat: add task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,10 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  if(!Array.isArray(arr)) return false;
+  let onlyStr = arr.filter(e=> {
+    if(typeof e === 'string') return e;
+  })
+  let noSpaces = onlyStr.join(',').replace(/\s/g, '').split(',');
+  let firstSymb = noSpaces.map(e=>e.slice(0,1).toUpperCase());
+  let sortArr = firstSymb.sort();
+  return result = sortArr.join('');
 };
